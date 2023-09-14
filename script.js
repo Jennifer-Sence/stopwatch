61db358a822416c0668d2e3feb14458600e4f016
Jennifer Sence <sencejenni@gmail.com>
2023-09-14 13:06:02 +0100

    DOM manipulation

---
+++ script.js
@@ -0,0 +1,5 @@
+const timerEl = document.getElementById("timer");
+const startButtonEL = document.getElementById("start");
+const stopButtonEL = document.getElementById("stop");
+const resetButtonEL = document.getElementById("reset");
+
